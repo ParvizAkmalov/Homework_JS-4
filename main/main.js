2b030319c8f0c88aaf84e20b0d42aaf2accebbe0
Parviz_8901 <parvizakmalov39@gmail.com>
2024-03-02 09:37:18 +0500

    Lessom js 4

---
+++ main/main.js
@@ -0,0 +1,85 @@
+console.log("                                            1-Misol");
+
+let r = +prompt("Birinchi son")
+let s = +prompt("Ikkinchi son")
+let f = +prompt("Javob")
+
+let y = r + s
+
+if (f === y) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// 2
+console.log("                                            2-Misol");
+
+let k = +prompt("Birinchi son")
+let a = +prompt("Ikkinchi son")
+let j = +prompt("Uchinchi son")
+let b = +prompt("Javob")
+
+let p = (k + a) * j
+
+if (b === p) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// 3
+console.log("                                            3-Misol");
+
+let q = +prompt("Birinchi son")
+let w = +prompt("Ikkinchi son")
+let e = +prompt("Uchinchi son")
+let t = +prompt("To'rtinchi son")
+let i = +prompt("Javob")
+
+let g = (q * w) - (e - t)
+
+if (i === g) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+
+// 4
+console.log("                                            4-Misol");
+
+let qr = +prompt("Birinchi son")
+let wr = +prompt("Ikkinchi son")
+let er = +prompt("Uchinchi son")
+let tr = +prompt("To'rtinchi son")
+let te = +prompt("Beshinchi son")
+let ir = +prompt("Javob")
+
+let fd = (qr * wr) - (er - tr) + (te)
+
+if (ir === fd) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+
+// 5
+console.log("                                            5-Misol");
+
+let c = +prompt("Birinchi son")
+let v = +prompt("Ikkinchi son")
+let z = +prompt("Uchinchi son")
+let x = +prompt("To'rtinchi son")
+let h = +prompt("Beshinchi son")
+let m = +prompt("Oltinchi son")
+let l = +prompt("Javob")
+
+let o = (c + v) + (z * x) + (h - m)
+
+if (l === o) {
+    console.log("True");
+} else {
+    console.log("False");
+}
